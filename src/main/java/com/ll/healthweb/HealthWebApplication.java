b86fb5ccfa23aa42c1a2b645e49cfa9be4ad74a2
kds98 <skyudae@naver.com>
2024-03-18 13:43:53 +0900

    setting

--- src/main/java/com/ll/healthweb/HealthWebApplication.java
+++ src/main/java/com/ll/healthweb/HealthWebApplication.java
@@ -2,8 +2,9 @@ package com.ll.healthweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class HealthWebApplication {
 
     public static void main(String[] args) {
