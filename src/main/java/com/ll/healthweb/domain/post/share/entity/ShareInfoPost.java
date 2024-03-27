271b1f8ff5f9b882e12cad77e74866d107db0b20
kds98 <102494128+kds98@users.noreply.github.com>
2024-03-27 21:32:07 +0900

    Kds98 7 (#12)
    * gitignore 추가
    
    * refactoring

---
+++ src/main/java/com/ll/healthweb/domain/post/share/entity/ShareInfoPost.java
@@ -0,0 +1,43 @@
+package com.ll.healthweb.domain.post.share.entity;
+
+import com.ll.healthweb.domain.comment.share.entity.Comment;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ShareInfoPost {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "member_id")
+//    private member memberId;
+
+    @Column(length = 200)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+    private LocalDateTime modifyDate;
+//    private Integer view;
+//    private Category categoryId;
+//    private Integer likes;
+
+    @OneToMany(mappedBy = "shareInfoPost", cascade = CascadeType.REMOVE)
+    private List<Comment> commentList;
+
+}
